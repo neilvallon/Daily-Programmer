4d19ad17e911542524e6ec615694de1333b21c12
Neil Vallon <neilvallon@gmail.com>
2014-05-16 17:23:15 -0500

    Parse dictionary size
    No idea why I decided to do this as a parser instead of spitting lines
    but here we Go.

--- novel-compression/novel/decompression.go
+++ novel-compression/novel/decompression.go
@@ -1,7 +1,52 @@
 package novel
 
-import ()
+import (
+	"fmt"
+	"strconv"
+)
 
-func Decompress(in string) (string, error) {
-	return in, nil
+func Decompress(in string) (str string, err error) {
+	d := &decompressor{data: in}
+
+	_, err = d.readInt()
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+type decompressor struct {
+	data  string
+	start int
+	pos   int
+}
+
+func (d *decompressor) next() (b byte) {
+	b = d.data[d.pos]
+	d.pos++
+	return
+}
+
+func (d *decompressor) back() {
+	d.pos--
+}
+
+func (d *decompressor) readInt() (i int, err error) {
+	for b := d.next(); '0' <= b && b <= '9'; b = d.next() {
+	}
+	d.back()
+
+	intS := d.data[d.start:d.pos]
+	if intS == "" {
+		err = fmt.Errorf("Could not read Int at position: %d", d.start)
+		return
+	}
+
+	i, err = strconv.Atoi(intS)
+	if err == nil {
+		d.start = d.pos
+	}
+
+	return
 }
